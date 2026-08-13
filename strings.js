d9348505325e877903afcdc23556518880de123c
OgbuEsther <ogbuuzoma413@gmail.com>
2026-08-13 21:01:22 +0100

    string methods

--- strings.js
+++ strings.js
@@ -20,3 +20,13 @@ console.log(filename.startsWith("report")); // true
 console.log(filename.startsWith("draft"));  // false
 console.log(filename.endsWith(".pdf"));     // true
 console.log(filename.endsWith(".docx"));    // false
+
+
+const extractExample = "Esther"
+console.log(extractExample.slice(0, 3)); // "Est"
+console.log(extractExample.slice(3));    // "her"
+console.log(extractExample.slice(-3));  // "her"
+console.log(extractExample.substring(0, 3)); // "Est"
+console.log(extractExample.substring(3));    // "her"
+console.log(extractExample.substring(-3));  // "Esther" (negative index treated as 0)
+
